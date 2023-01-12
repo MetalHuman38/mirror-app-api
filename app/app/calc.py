7400a9d9605beee9bedab02f4e8432e074d204ec
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2023-01-12 05:17:32 +0000

    Added Github Actions.

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,23 +1,15 @@
 """
 calculator function
-
 """
+
 
 def add(x,y):
-
-    """Add x + y and return the result."""
+    """Add x and y and return the result."""
     return x + y
 
-"""
-Add x + y and return the result.
-
-"""
 
 def subtract(x,y):
-    
-    """Subtract x + y and return the result."""
-
-
+    """Subtract x from y and return the result."""
     return x-y
 
 
--- app/app/test.py
+++ app/app/test.py
@@ -1,24 +1,24 @@
 """
 sample test
-
 """
-
 from django.test import SimpleTestCase
 
 from app import calc 
+
 
 class calcTests(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_number(self):
-        """test adding numbers together"""
-
-        res = calc.add(5,6)
+        """test adding numbers together."""
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self): 
-        """Test subtracting numbers."""
-        res = calc.subtract(10,5)
+        """test subtracting numbers together."""
+        res = calc.subtract(10, 5)
+
         self.assertEqual(res, 5)
+
 
