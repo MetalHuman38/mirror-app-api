63281a5bea899279fda70d75e4424fdf7df4f031
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2023-01-12 05:30:40 +0000

    Added Github Actions.

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,12 +3,11 @@ calculator function
 """
 
 
-def add(x,y):
+def add(x, y):
     """Add x and y and return the result."""
     return x + y
 
 
-def subtract(x,y):
+def subtract(x, y):
     """Subtract x from y and return the result."""
-    return x-y
-
+    return x - y
