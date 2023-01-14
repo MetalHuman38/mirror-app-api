742afc46b34d3c4bb673bdc6bbab80bff813c7b4
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2023-01-14 23:10:55 +0000

    Added Github Actions

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -17,7 +17,6 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
-
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
@@ -32,3 +31,4 @@ class CommandTests(SimpleTestCase):
         
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
+
