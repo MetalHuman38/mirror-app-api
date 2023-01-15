b44c117452b009caac871a9aae5fd3a03232a41f
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2023-01-15 01:26:09 +0000

    Creating Custom User Model

--- app/core/models.py
+++ app/core/models.py
@@ -3,14 +3,15 @@ Database model
 """
 from django.db import models
 from django.contrib.auth.models import(
-  AbstractBaseUser,
-  BaseUserManager,
-  PermissionsMixin,
-
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for User."""
+
 
     def create_user(self, email, password=None, **extra_fields):
         user = self.model(email=email, **extra_fields)
