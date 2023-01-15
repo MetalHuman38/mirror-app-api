2cbc9456bd469512f9a5be2fc888c0446e6a90a0
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2023-01-15 12:56:15 +0000

    Setting up Django Admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,30 +15,31 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
       (None, {'fields': ('email', 'password')}),
       (
-        _('Permissions'),
-        {
-            'fields': (
-              'is_active',
-              'is_staff',
-              'is_superuser',
-          )
-        }
-      ),
-      (_('Important dates'),{'fields': ('last_login',)}),
+          _('Permissions'),
+          {
+              'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+              )
+            }
+          ),
+          (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
     add_fieldsets = (
-      (None, {
-        'classes': ('wide',),
-        'fields': (
-            'email',
-            'password1',
-            'password2',
-            'name',
-            'is_active',
-            'is_superuser'
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_superuser'
+              )
+          }),
         )
-      }),
-    )
+
 
 admin.site.register(models.User, UserAdmin)
