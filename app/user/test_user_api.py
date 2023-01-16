2f7e47c710b04a6161c29c4bdd83175359d089c7
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2023-01-16 08:53:29 +0000

    Implementing Create User API

--- app/user/test_user_api.py
+++ app/user/test_user_api.py
@@ -4,26 +4,25 @@ Test for the user API.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
 from rest_framework.test import APIClient
 from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
 
+
 def create_user(**params):
     """Create and return user."""
     return get_user_model().objects.create_user(**params)
-
 
 
 class PublicUserApiTests(TestCase):
     """Test the public features of the user API."""
 
     def setUp(self):
-          self.client = APIClient()
+        self.client = APIClient()
 
-    def  test_create_user_success(self):
+    def test_create_user_success(self):
         """Test creating user is successful"""
         payload = {
             'email': 'test@example.com',
