bd472e268c35df35c38525119d0a16a088029431
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2023-01-18 01:18:25 +0000

    Write Test for recipe model

--- app/core/admin.py
+++ app/core/admin.py
@@ -13,18 +13,18 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-      (None, {'fields': ('email', 'password')}),
-      (
-          _('Permissions'),
-          {
-              'fields': (
-                  'is_active',
-                  'is_staff',
-                  'is_superuser',
-              )
-          }
-      ),
-      (_('Important dates'), {'fields': ('last_login',)}),
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
     add_fieldsets = (
@@ -44,3 +44,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
